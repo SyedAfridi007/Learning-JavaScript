15f3029d78b02c312597982583fa631103aa07b9
Md. Syed Afridi <83403859+SyedAfridi007@users.noreply.github.com>
2022-05-21 23:43:29 +0600

    Add files via upload

---
+++ Basic JavaScript/70 ES6 Modules/index.js
@@ -0,0 +1,14 @@
+/*
+import {text, setText} from './module.js';
+
+console.log(text);
+
+setText("Goodbye From Es6");
+console.log(text);
+
+*/
+/*/
+import {message} from './module.js';
+console.log(message);
+
+*/
---
+++ Basic JavaScript/81 JSON Path , Access,Modify,Delete and Conversion/index.js
@@ -0,0 +1,56 @@
+//run terminal node index.js
+//cls to clear
+
+
+const data = require("./index.json");
+
+//console.log(data);
+//console.log(data.students[0]);
+//console.log(data.students[0].name);
+//console.log(data.students[0].languages)
+//console.log(data.students[0].languages[0])
+//console.log(data.students[0].friends);
+//console.log(data.students[0].friends[0]);
+//console.log(data.students[0].friends[0].name);
+
+//Access and modify
+//data.students[0].name = "Syed";
+//console.log(data.students[0].name);
+
+
+//Delete 
+//delete data.students[0].name;
+//console.log(data.students[0]);
+
+//data.students[0].name = "Afridi";
+//console.log(data.students[0]);
+
+
+
+/*using loop
+for( x in data){
+    //console.log(x)
+    //console.log(data[x]);
+}
+*/
+
+/*
+const data2 = {
+    name: "Rafiq",
+    age: 25
+}
+*/
+
+//console.log(typeof(data2));
+//console.log(typeof(JSON.stringify(data2)));
+//console.log(JSON.stringify(data2));
+//console.log(JSON.parse('{"name" : "anis"}'));
+
+
+
+
+
+
+
+
+
